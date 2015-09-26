c2a1e4ae72a9f93c3219c2ff8f317ac1a896e935
Harun Smrkovic <harun@personal.com>
2015-09-26 15:22:50 +0200

    small debug change

--- app/scripts/controllers/debugctrl.js
+++ app/scripts/controllers/debugctrl.js
@@ -10,7 +10,7 @@
 angular.module('superbCalendar')
   .controller('debugCtrl', function($scope, $log){
     $scope.bookingCalendar = {
-      busyDates: [{startDate: "2015-09-25",endDate: "2015-09-26"},{date: "2015-09-29"},{startDate: "2015-09-30",endDate: "2015-10-08"}],
+      busyDates: [{startDate: "2015-09-25",endDate: "2015-09-26"},{date: "2015-09-14"},{startDate: "2015-09-30",endDate: "2015-10-08"}],
       dates: []
     };
   });
